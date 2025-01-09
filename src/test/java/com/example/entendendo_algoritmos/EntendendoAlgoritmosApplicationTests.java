300227cdaa73c6a3ae5e2ac2f37bef63fa76186b
Leidiane Aguiar <leidianec.aguiar@gmail.com>
2025-01-09 08:09:45 -0300

    first commit

---
+++ src/test/java/com/example/entendendo_algoritmos/EntendendoAlgoritmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entendendo_algoritmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntendendoAlgoritmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
